d37ef50e8c14c83fca45c57a46b568e6a2bf3f1b
Marguerite Su <i@marguerite.su>
2012-11-26 21:16:50 +0800

    final

--- spotify-rpm-generator.sh
+++ spotify-rpm-generator.sh
@@ -9,7 +9,7 @@
 
 if [[ $UID -ne 0 ]]
 then
-        echo "Only root can run this script!"
+        echo -e "\033[38;5;148mOnly root can run this script!\033[39m"
         exit 1
 fi
 
@@ -25,78 +25,78 @@ else
 	RPM_ARCH=i586
 fi
 
+NORMAL_USER=`logname`
+
 # check if we have 'rpmbuild' package installed, if not then install it.
 
-if [[ `rpm -qa rpm-build` != "" ]] ; then
-	zypper install --no-refresh rpm-build # 'rpm-build' is in oss, so no need to refresh that long.
+if [[ `rpm -qa rpm-build` == "" ]] ; then
+	zypper --no-refresh install rpm-build # 'rpm-build' is in oss, so no need to refresh that long.
 fi
 
 # prepare for the build environment.
 
 # download specfile from github
 cd /usr/src/packages/SPECS/
-wget https://raw.github.com/marguerite/opensuse-spotify-installer/master/spotify.spec
+wget https://raw.github.com/marguerite/opensuse-spotify-installer/master/spotify.spec -O spotify.spec
 
 # download source deb
 cd ../SOURCES/
 
-echo "If you already have spotify deb, please press 'ctrl + c',\n
-put it under your home, and restart this script.\n
-or else we'll download it (may take a long time)."
+echo -e "\033[38;5;148mIf you already have spotify deb, please press 'ctrl + c',
+put it under your home, and restart this script.(ignore if it's already done.)
+or else we'll download it (may take a long time).\033[39m"
 
 sleep 5
 
-echo "Downloading..."
+echo -e "\033[38;5;148mDownloading...\033[39m"
 	
-test -x spotify-client_${SPOTIFY_VERSION}_$SPOTIFY_ARCH.deb || wget http://repository.spotify.com/pool/non-free/s/spotify/spotify-client_${SPOTIFY_VERSION}_$SPOTIFY_ARCH.deb
+test -e /home/$NORMAL_USER/spotify-client_${SPOTIFY_VERSION}_$SPOTIFY_ARCH.deb && cp -r /home/$NORMAL_USER/spotify-client_${SPOTIFY_VERSION}_$SPOTIFY_ARCH.deb ./ || wget -c http://repository.spotify.com/pool/non-free/s/spotify/spotify-client_${SPOTIFY_VERSION}_$SPOTIFY_ARCH.deb
 
 # build
 
-echo "Building..."
+echo -e "\033[38;5;148mBuilding...\033[39m"
 
 cd ../SPECS/
 rpmbuild -ba spotify.spec
 
-echo "Build done! Cleaning..."
+echo -e "\033[38;5;148mBuild done! Cleaning...\033[39m"
 
 # clean
 
 ## copy generated rpm
 
-NORMAL_USER=`logname`
-
 cp -r ../RPMS/$RPM_ARCH/*.rpm /home/$NORMAL_USER/
      
 ## real clean
-rm -rf /usr/src/packages/SOURCES/*
-rm -rf /usr/src/packages/BUILD/*
+rm -rf /usr/src/packages/SOURCES/spotify-*.deb
+rm -rf /usr/src/packages/BUILD/spotify-*
 rm -rf /usr/src/packages/BUILDROOT/*
-rm -rf /usr/src/packages/RPM/$RPM_ARCH/*
-rm -rf /usr/src/packages/SRPM/*
-rm -rf /usr/src/packages/SPECS/*
+rm -rf /usr/src/packages/RPM/$RPM_ARCH/spotify-*.rpm
+rm -rf /usr/src/packages/SRPM/spotify-*.rpm
+rm -rf /usr/src/packages/SPECS/spotify.spec
 
 # install
 
 ## dependencies
 
-echo "Resolving dependencies..."
+echo -e "\033[38;5;148mResolving dependencies...\033[39m"
 
 ! [ `rpm -qa mozilla-nss` ] && zypper --no-refresh install mozilla-nss
 ! [ `rpm -qa mozilla-nspr` ] && zypper --no-refresh install mozilla-nspr
 ! [ `rpm -qa libopenssl1_0_0` ] && zypper --no-refresh install libopenssl1_0_0
 
-echo "Installing..."
+echo -e "\033[38;5;148mInstalling...\033[39m"
 
 rpm -ivh --force --nodeps /home/$NORMAL_USER/spotify-*.rpm
 
-echo "Congrats! Installation finished.\n
-We put the generated RPM under your home.\n
-Next time you can use `sudo rpm -ivh --force --nodeps spotify-*.rpm` or\n
-`sudo zypper --no-refresh install --force-resolution` to install it."
+echo -e "\033[38;5;148mCongrats! Installation finished.
+We put the generated RPM under your home.
+Next time you can use 'sudo rpm -ivh --force --nodeps spotify-*.rpm' or
+'sudo zypper --no-refresh install --force-resolution' to install it.\033[39m"
 
 # quit
 
-echo "Quitting..."
+echo -e "\033[38;5;148mQuitting...\033[39m"
 
 exit 0
 
